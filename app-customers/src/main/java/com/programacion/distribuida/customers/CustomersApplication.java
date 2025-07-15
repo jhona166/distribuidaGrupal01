fbdda59490afe6a8ff6c5e4804fdadbfbd9e0112
Jhonatan Altamirano <jdaltamiranoo1@uce.edu.ec>
2025-07-15 03:19:00 -0500

    Se_ver_intera_Se_aniade_custov1.1

---
+++ app-customers/src/main/java/com/programacion/distribuida/customers/CustomersApplication.java
@@ -0,0 +1,11 @@
+package com.programacion.distribuida.customers;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class CustomersApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(CustomersApplication.class, args);
+
+    }
+}
